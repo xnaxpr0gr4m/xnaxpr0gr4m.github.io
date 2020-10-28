77332cf9f101862cceeedc58b114fe80ec33d62a
xnaxpr0gr4m <73607385+xnaxpr0gr4m@users.noreply.github.com>
2020-10-28 20:09:22 +0200

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+$config = array(
+  'title' => 'Pixel - Digital Agency HTML Template',
+  'db' => array(
+          'server' => 'localhost',
+          'username' => 'mysql',
+          'password' => 'mysql',
+          'name' => 'test_db'
+          )
+);
